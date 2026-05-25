0570620d073ac4800373df215bc08930bddca510
吉川耕平 <56056670+y-kohei1228@users.noreply.github.com>
2026-05-25 17:06:18 +0900

    Add user following (#10)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -87,4 +87,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
+  test "should redirect following when not logged in" do
+    get following_user_path(@admin)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@admin)
+    assert_redirected_to login_url
+  end
 end
