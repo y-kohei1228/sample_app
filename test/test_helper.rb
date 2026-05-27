f15f2abacf35dab0fffd809e9bb4c1dad0eb0ad3
吉川耕平 <56056670+y-kohei1228@users.noreply.github.com>
2026-05-27 09:13:38 +0000

    Integrate SimpleCov into CI workflow.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,11 @@
+require "simplecov"
+SimpleCov.start "rails"
+
+SimpleCov.start "rails" do
+  enable_coverage :branch
+  minimum_coverage 80
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
